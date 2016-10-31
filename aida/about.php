74829123f136181e954b83b5ecf8773695428d40
Simeon Lees <simeon.lees@hotmail.com>
2016-10-31 13:27:00 +0000

    Added about page

---
+++ aida/about.php
@@ -0,0 +1,18 @@
+<?php
+$title = "About";
+require_once 'config/init.php';
+include 'template/header.php';
+include 'template/nav.php';
+include 'template/content-box.php';
+?>
+<!-- CONTENT START -->
+
+<h1>About CityBase</h1>
+
+<p>CityBase seeks to find you the perfect Flat, Apartment or Penthouse for your stay across 40 major cities all over the world.</p>
+
+  <!-- CONTENT END -->
+<?php
+include 'template/content-footer.php';
+include 'template/footer.php';
+?>
